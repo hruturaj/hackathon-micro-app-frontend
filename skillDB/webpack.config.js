73e7e96c9264552e5b44db8a49fba476d00f5fa3
Hruturaj Nikam <hrutunikam007@gmail.com>
2023-01-07 23:05:01 +0530

    webpack for app1

--- skillDB/webpack.config.js
+++ skillDB/webpack.config.js
@@ -1,14 +1,14 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
-const deps = require('./package.json').dependencies;
+const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: 'http://localhost:8082/',
+    publicPath: "http://localhost:8082/",
   },
 
   resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json', '.scss'],
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json", ".scss"],
   },
 
   devServer: {
@@ -20,20 +20,20 @@ module.exports = {
     rules: [
       {
         test: /\.m?js/,
-        type: 'javascript/auto',
+        type: "javascript/auto",
         resolve: {
           fullySpecified: false,
         },
       },
       {
         test: /\.(css|s[ac]ss)$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
         test: /\.(ts|tsx|js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
     ],
@@ -41,15 +41,17 @@ module.exports = {
 
   plugins: [
     new ModuleFederationPlugin({
-      name: 'skillDB',
-      filename: 'remoteEntry.js',
+      name: "skillDB",
+      filename: "remoteEntry.js",
       remotes: {
         // home: 'skillDB@http://localhost:8082/remoteEntry.js',
-        app2: 'skillList@http://localhost:8081/remoteEntry.js',
+        app2: "skillList@http://localhost:8081/remoteEntry.js",
       },
       exposes: {
-        './Header': './src/components/Header/index.tsx',
-        './routes': './src/routes',
+        "./Header": "./src/components/Header/index.tsx",
+        "./routes": "./src/routes",
+        "./Login": "./src/components/Auth/Login/index.tsx",
+        "./Register": "./src/components/Auth/Register/index.tsx",
       },
       shared: {
         ...deps,
@@ -57,14 +59,14 @@ module.exports = {
           singleton: true,
           requiredVersion: deps.react,
         },
-        'react-dom': {
+        "react-dom": {
           singleton: true,
-          requiredVersion: deps['react-dom'],
+          requiredVersion: deps["react-dom"],
         },
       },
     }),
     new HtmlWebPackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
   ],
 };
